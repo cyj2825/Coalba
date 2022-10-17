f6a73e307a6611187abd1e9349c4e8c66c9bf2d9
cyj2825 <cyj2825@naver.com>
2022-10-17 15:03:13 +0900

    bottom navigation bar 구현

--- app/src/main/java/com/example/coalba/MainActivity.kt
+++ app/src/main/java/com/example/coalba/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.coalba
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.coalba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +13,16 @@ class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
     private val binding get() = mBinding!!
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 바인딩
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController= Navigation.findNavController(this,R.id.main_nav_host_fragment)
+        setupWithNavController(binding.bnv,navController)
     }
 
     // 액티비티가 파괴될 때..
